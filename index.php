f031560ab60f73ad986f4c8ebbcfcc6cb2f61cd6
Govind <govindkotalwargk@gmail.com>
2026-01-14 01:41:41 +0530

    update2

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo "PHP App Running via EKS Ingress";
+echo "<!DOCTYPE html>
+<html>
+<head>
+    <title>Love Page</title>
+    <style>
+        body {
+            background-color: #fce4ec;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: #d81b60;
+            font-size: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Kartthik ❤️ Dhanu</h1>
+</body>
+</html>";
 ?>
